4c909f51a9c7294f3d8dbeaa5504853bf2b6cf3d
Thomas Savasten <thomas@savasten.com>
2025-11-08 16:59:28 -0600

    initial push

---
+++ arduino/src/main.ino
@@ -0,0 +1,17 @@
+#include "DFRobot_Heartrate.h"
+
+DFRobot_Heartrate heartrate(DIGITAL_MODE);
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  uint8_t rateValue;
+  heartrate.getValue(A1);
+  rateValue = heartrate.getRate()
+  if(rateValue){
+    Serial.println(rateValue);
+  }
+  delay(20);
+}
