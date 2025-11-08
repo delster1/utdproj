23b16d87f1aa6f28f9114b3ade431debd2b7b5f5
Thomas Savasten <thomas@savasten.com>
2025-11-08 17:45:44 -0600

    updated platform io

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -5,18 +5,16 @@ DFRobot_Heartrate heartrate(DIGITAL_MODE);
 
 void setup() {
   Serial.begin(115200);
-
-
 }
 
 void loop() {
   uint8_t rateValue;
   heartrate.getValue(A1);
-  rateValue = heartrate.getRate()
+  rateValue = heartrate.getRate();
   if(rateValue){
     Serial.println(rateValue);
   }
-  delay(20);
+  delay(100);
 
 
 
