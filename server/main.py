8cd7d6362a7d6613833d3f7c220a94c86b2831bc
Thomas-writes <thomas@savasten.com>
2025-11-09 04:01:53 -0600

    changes

--- server/main.py
+++ server/main.py
@@ -1,11 +1,12 @@
 from serial_to_JSON import serial_to_JSON
 from w2db import write2redis
+import serial
 
 def main():
     '''RUN AND READ HERE'''
     ser = serial.Serial("/dev/ttyACM0", 115200, timeout=1)
     while True:
-        line = ser.readline().decode("utf-9").strip()
+        line = ser.readline().decode("utf-8").strip()
         if not line:
             continue
         else:
--- server/serial_to_JSON.py
+++ server/serial_to_JSON.py
@@ -1,15 +1,13 @@
-import serial
 import json 
 import requests
 
 def serial_to_JSON(data):
     try:
         name, value = data.split(":")
-        new_data =
-            {name.strip():str(float(value.strip()))}
+        new_data = {name.strip():str(float(value.strip()))}
         json_str = json.dumps(new_data)
         print(json_str)
         return json_str
     except ValueError:
         print(f"Skipping invalid line: {data}")
-        return NULL
+        return None
