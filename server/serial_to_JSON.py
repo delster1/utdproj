8f9ffb2a235121154b0febfd71fa781daa50e3ed
molly-mas <mollymasalskis@gmail.com>
2025-11-08 18:10:35 -0600

    serial to json maybe works

---
+++ server/serial_to_JSON.py
@@ -0,0 +1,27 @@
+import serial
+import json 
+
+
+def serial_to_JSON(data):
+    try:
+        name, value = data.split(":")
+        new_data = {
+            "Sensor:" : name.strip(),
+            "Value:" : float(value.strip())
+        }
+        json_str = json.dumps(new_data)
+        print(json_str)
+    except ValueError:
+        print(f"Skipping invalid line: {data}")
+
+
+def main():
+    '''RUN AND READ HERE'''
+    ser = serial.Serial("/dev/ttyUSB0", 115200, timeout=1)
+    while True:
+        line = ser.readline().decode("utf-8").strip()
+        if not line:
+            continue
+        else:
+            serial_to_JSON(line)
+main()
