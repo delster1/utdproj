4847884ce9abbb0a7b8935bc97b60d414a238973
Thomas Savasten <thomas@savasten.com>
2025-11-09 08:12:43 -0600

    hopefully pings work now

--- server/main.py
+++ server/main.py
@@ -1,6 +1,9 @@
 from serial_to_JSON import serial_to_JSON
 from w2db import write2redis
+from vibrate import start_vibes, ping_server
 import serial
+import time
+import requests
 
 def main():
     '''RUN AND READ HERE'''
@@ -13,5 +16,17 @@ def main():
             jsonline = serial_to_JSON(line)
         if jsonline:
             write2redis(jsonline)
+            
+        ser = serial.Serial("/dev/ttyACM1", 11520, timeout=1)
+        time.sleep(2)
+        
+        server_comf = ping_server("http://vibrator.d3llie.tech/vibrate")
+        if server_comf:
+            print("YAYYY IT WORKED")
+            start_vibes(ser)
+            time.sleep(2)
+        else:
+            print("It fr fr worked")
+            pass
 main()
 
